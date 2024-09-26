ac3e1bfcb4bd8185f5e0a4bb39510bbdd3492119
wareya <wareya@gmail.com>
2024-09-26 03:17:10 -0400

    benchmarkify

--- bbel/my_regex_tests.cpp
+++ bbel/my_regex_tests.cpp
@@ -7,13 +7,18 @@
 #include "my_regex.h"
 
 #include <chrono>
+
 #include <string>
 
 #define PCRE2_CODE_UNIT_WIDTH 8
 #include <pcre2.h>
 
+#define BE_QUIET
+
 void testify(void)
 {
+    using clock = std::chrono::high_resolution_clock;
+    
     static const char * regexes[] = {
         "(b|a|as|q|)*?X",
         "",
@@ -218,6 +223,7 @@ void testify(void)
         "test+acc@example.com",
         "test.acc@example.com",
         "test.acc.acc@sub.example.com",
+        "loooooooo10235699ng.1g.g.g.210g01.longie.acc@sub.example.com.co.co.uk.jp.fakedomain.loooooooooooooooooooonger.com......",
         "test.acc@sub.example.com",
         "test@sub.example.com",
         "@example.com",
@@ -290,6 +296,12 @@ void testify(void)
         "aaaaaabbbabaqa",
     };
     
+    const char * slowest_my_regex = "";
+    double slowest_my_regex_time = 0.0;
+    double total_my_regex_time = 0.0;
+    const char * slowest_pcre2_regex = "";
+    double slowest_pcre2_regex_time = 0.0;
+    double total_pcre2_regex_time = 0.0;
     
     for (size_t i = 0; i < sizeof(regexes) / sizeof(regexes[0]); i++)
     //for (size_t i = 0; 0; i++)
@@ -299,8 +311,18 @@ void testify(void)
         RegexToken tokens[256];
         memset(tokens, 0xFF, sizeof(tokens));
         int16_t token_count = sizeof(tokens)/sizeof(tokens[0]);
+        
+        auto start = clock::now();
         int e = regex_parse(regex, tokens, &token_count, 0);
+        double t = std::chrono::duration_cast<std::chrono::microseconds>(clock::now() - start).count() / 1000000.0;
         assert(!e);
+        
+        total_my_regex_time += t;
+        if (t > slowest_my_regex_time)
+        {
+            slowest_my_regex_time = t;
+            slowest_my_regex = regex;
+        }
         
         bool has_possessive = false;
         for (int32_t n = 0; n < token_count; n++)
@@ -312,50 +334,71 @@ void testify(void)
             }
         }
         
+        #ifndef BE_QUIET
         printf("token count: %d\n", token_count);
         print_regex_tokens(tokens);
+        printf("Took %f seconds for my regex engine to parse the regex\n", t);
+        #endif
         
         std::string regex_str = regex;
         
         int errorcode;
         PCRE2_SIZE erroroffset;
+        start = clock::now();
         pcre2_code * re = pcre2_compile(PCRE2_SPTR8(regex), PCRE2_ZERO_TERMINATED,
             PCRE2_ANCHORED | PCRE2_NO_UTF_CHECK | PCRE2_DOTALL | PCRE2_NO_AUTO_POSSESS | PCRE2_NO_DOTSTAR_ANCHOR | PCRE2_NO_START_OPTIMIZE,
             &errorcode, &erroroffset, NULL);
+        t = std::chrono::duration_cast<std::chrono::microseconds>(clock::now() - start).count() / 1000000.0;
+        #ifndef BE_QUIET
+        printf("Took %f seconds for my PCRE2 to compile the regex\n", t);
+        #endif
+        if (t > slowest_pcre2_regex_time)
+        {
+            slowest_pcre2_regex_time = t;
+            slowest_pcre2_regex = regex;
+        }
+        total_pcre2_regex_time += t;
         
         for (size_t j = 0; j < sizeof(texts) / sizeof(texts[0]); j++)
         {
             const char * text = texts[j];
             std::string text_str = text;
             
-            using clock = std::chrono::high_resolution_clock;
-            
             int64_t pcre2_len = -1;
             
+            #ifndef BE_QUIET
             printf("testing PCRE2 regex `%s` on string `%s`...\n", regex, text);
             fflush(stdout);
+            #endif
             
             auto start = clock::now();
             pcre2_match_data * match_data = pcre2_match_data_create_from_pattern(re, 0);
-            int submatch_count = pcre2_match(re, PCRE2_SPTR8(text), text_str.size(), 0, PCRE2_ANCHORED | PCRE2_NO_UTF_CHECK, match_data, 0); 
+            int submatch_count = pcre2_match(re, PCRE2_SPTR8(text), text_str.size(), 0, PCRE2_ANCHORED | PCRE2_NO_UTF_CHECK, match_data, 0);
+             
+            #ifndef BE_QUIET
             double t = std::chrono::duration_cast<std::chrono::microseconds>(clock::now() - start).count() / 1000000.0;
+            #endif
             
             //printf("submatch count: %d\n", submatch_count);
             //printf("ovector count: %d\n", pcre2_get_ovector_count(match_data));
             
-            PCRE2_SIZE * ovector;
+            PCRE2_SIZE * ovector = 0;
             if (submatch_count > 0)
             {
                 ovector = pcre2_get_ovector_pointer(match_data);
                 size_t offs = ovector[0];
                 if (offs == 0)
                     pcre2_len = ovector[1] - offs;
+                #ifndef BE_QUIET
                 printf("pcre2 regex found match at %zd with len %zd after %f seconds\n", offs, pcre2_len, t);
+                #endif
             }
+            #ifndef BE_QUIET
             else
                 printf("pcre2 regex found no match after %f seconds\n", t);
             
             printf("testing my regex `%s` on string `%s`...\n", regex, text);
+            #endif
             
             start = clock::now();
             
@@ -365,23 +408,29 @@ void testify(void)
             memset(cap_span, 0xFF, sizeof(cap_span));
             
             int64_t match_len = regex_match(tokens, text, 0, 16, cap_pos, cap_span);
-            t = std::chrono::duration_cast<std::chrono::microseconds>(clock::now() - start).count() / 1000000.0;
             
             assert(match_len != -3);
+            #ifndef BE_QUIET
+            t = std::chrono::duration_cast<std::chrono::microseconds>(clock::now() - start).count() / 1000000.0;
             if (match_len >= 0)
                 printf("my regex found match with len %zd after %f seconds\n", match_len, t);
             else if (match_len == -2)
                 printf("my regex ran out of memory after %f seconds (note: `%s`)\n", t, regex);
             else
                 printf("my regex found no match after %f seconds\n", t);
+            #endif
             
             // we define captures differently than PCRE2 for possessives, so skip them
             if (!has_possessive && submatch_count > 0)
             {
+                #ifndef BE_QUIET
                 printf("comparing %zd to %zd...\n", match_len, pcre2_len);
                 printf("regex `%s`, string `%s`\n", regex, text);
+                #endif
                 assert(match_len == pcre2_len);
+                #ifndef BE_QUIET
                 puts("comparing captures...");
+                #endif
                 if (match_len >= 0)
                 {
                     for (int x = 0; x < submatch_count && x < 16; x++)
@@ -391,7 +440,9 @@ void testify(void)
                         {
                             size_t pcre2_len = ovector[x*2+1] - where;
                             // probably a situation of std capturing a zero-length group repetition
+                            #ifndef BE_QUIET
                             printf("Capture %d: std (%zd,%zd)  mine (%zd,%zd)\n", x, where, pcre2_len, cap_pos[x], cap_span[x]);
+                            #endif
                             if (!(cap_pos[x] == -1 && cap_span[x] == -1 && where == 0 && pcre2_len == 0))
                             {
                                 assert(where     == (size_t)cap_pos[x]);
@@ -406,6 +457,12 @@ void testify(void)
         }
         pcre2_code_free(re);
     }
+    
+    printf("Slowest regex for me to parse at %f seconds:\n%s\n", slowest_my_regex_time, slowest_my_regex);
+    printf("Slowest regex for pcre2 to parse at %f seconds:\n%s\n", slowest_pcre2_regex_time, slowest_pcre2_regex);
+    
+    printf("Total parse time for me: %f\n", total_my_regex_time);
+    printf("Total parse time for pcre2: %f\n", total_pcre2_regex_time);
     
     RegexToken tokens[256];
     int16_t token_count = sizeof(tokens)/sizeof(tokens[0]);
@@ -431,6 +488,47 @@ void testify(void)
     print_regex_tokens(tokens);
     
     puts("All regex tests passed!");
+    
+    if (1)
+    {
+        puts("Microbenchmark: matching `\\.\\d+|\\d+\\.\\d*` against 3.1415926535 one million times...");
+        
+        RegexToken tokens[256];
+        int16_t token_count = sizeof(tokens)/sizeof(tokens[0]);
+        
+        int e = regex_parse("\\.\\d+|\\d+\\.\\d*", tokens, &token_count, 0);
+        assert(!e);
+        auto start = clock::now();
+        for (size_t i = 0; i < 1000000; i++)
+        {
+            int64_t matchlen = regex_match(tokens, "3.1415926535", 0, 0, 0, 0);
+            assert(matchlen == 12);
+            volatile int64_t a = 0; matchlen = a; // force the loop to not be optimized away
+        }
+        double t = std::chrono::duration_cast<std::chrono::microseconds>(clock::now() - start).count() / 1000000.0;
+        printf("Match time for me: %f\n", t);
+        
+        
+        int errorcode;
+        PCRE2_SIZE erroroffset;
+        pcre2_code * re = pcre2_compile(PCRE2_SPTR8("\\.\\d+|\\d+\\.\\d*"), PCRE2_ZERO_TERMINATED,
+            PCRE2_ANCHORED | PCRE2_NO_UTF_CHECK | PCRE2_DOTALL | PCRE2_NO_AUTO_POSSESS | PCRE2_NO_DOTSTAR_ANCHOR | PCRE2_NO_START_OPTIMIZE,
+            &errorcode, &erroroffset, NULL);
+        pcre2_match_data * match_data = pcre2_match_data_create_from_pattern(re, 0);
+        PCRE2_SIZE * ovector = pcre2_get_ovector_pointer(match_data);
+        
+        start = clock::now();
+        size_t size = strlen("3.1415926535");
+        for (size_t i = 0; i < 1000000; i++)
+        {
+            int submatch_count = pcre2_match(re, PCRE2_SPTR8("3.1415926535"), size, 0, PCRE2_ANCHORED | PCRE2_NO_UTF_CHECK, match_data, 0);
+            int64_t matchlen = ovector[1] - ovector[0];
+            assert(submatch_count == 1 && matchlen == 12);
+            volatile int64_t a = 0; matchlen = a; // force the loop to not be optimized away
+        }
+        t = std::chrono::duration_cast<std::chrono::microseconds>(clock::now() - start).count() / 1000000.0;
+        printf("Match time for pcre2: %f\n", t);
+    }
 }
 
 int main(void)
