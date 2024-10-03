4e8d9f481e54f6aac311a8c07f78b8d032c2c89c
wareya <wareya@gmail.com>
2024-10-03 02:11:09 -0400

    fix run_tests.sh on linux

--- run_tests.sh
+++ run_tests.sh
@@ -5,10 +5,14 @@ case "$OSTYPE" in
   *)        f=tests.out ;;
 esac
 case "$OSTYPE" in
+  msys*)    std=c99 ;;
+  *)        std=gnu99 ;; # functions like fdopen aren't exported by the standard library unless compiling in GNU mode
+esac
+case "$OSTYPE" in
   msys*)    asan="" ;;
   #*)        asan="-fsanitize=address" ;;
   *)        asan="-fsanitize=undefined -fno-sanitize=function" ;;
 esac
 
-clang --std=c99 src/tests.c -Wall -Wextra -pedantic -O0 -g -ggdb $asan -Wno-unused-function -o $f || exit 1
+clang --std=$std src/tests.c -Wall -Wextra -pedantic -O0 -g -ggdb $asan -Wno-unused-function -o $f || exit 1
 ./$f
